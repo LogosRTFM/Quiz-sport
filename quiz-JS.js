d4c6b49ef2cdf1fbf782a08e0e09057d2f18293a
Logos <42390852+LogosRTFM@users.noreply.github.com>
2022-05-17 23:46:30 +0200

    Update quiz-JS.js

--- quiz-JS.js
+++ quiz-JS.js
@@ -8,8 +8,25 @@
       <form name="f1" method="GET" action="reponse.php">
         <fielset>
           <legend>Quiz sport et culture</legend> 
-          <p>la Coupe du monde de football en 2018 ? 
+          <p>Qui a gagné la Coupe du monde de football en 2018 ? 
             <input type="radio" name="foot" value="belgique" checked> La Belgique 
             <input type="radio" name="foot" value="croatie"> La Croatie
           </p> 
-          <p>Quelle est la longueur maximale (en mètres) des épreuves de natation aux JO ? <input type="number" name="distance" required title="Saisissez ici une distance en mètres sans unités."> </p> <p>Quelle(s) invention(s) sont attribuée(s) à Léonard de Vinci ?<br/> <input type="checkbox" name="scaphandre">Le scaphandre<br/> <input type="checkbox" name="tiede">L’eau tiède<br/> <input type="checkbox" name="parachute">Le parachute<br/> </p> <p> <!-- Placer ici les boutons. --> </p>  </form> <script> </script> </body></html>
+          <p>Quelle est la longueur maximale (en mètres) des épreuves de natation aux JO ? 
+            <input type="number" name="distance" required title="Saisissez ici une distance en mètres sans unités."> 
+          </p> 
+          <p>Quelle(s) invention(s) sont attribuée(s) à Léonard de Vinci ?
+            <br/> 
+            <input type="checkbox" name="scaphandre">Le scaphandre<br/> 
+            <input type="checkbox" name="tiede">L’eau tiède<br/> 
+            <input type="checkbox" name="parachute">Le parachute<br/> 
+          </p> 
+          <p> 
+            <!-- Placer ici les boutons. --> 
+          </p>
+        </fielset>           
+      </form> 
+      <script> 
+      </script> 
+  </body>
+</html>
